c889a90f1892171ecc4053503733b6a4a6a5d458
Ignacio Gutierrez <nachojgutierrez@gmail.com>
2014-05-26 22:42:30 -0300

    changed to

--- js/app.js
+++ js/app.js
@@ -1,25 +1,21 @@
 (function(){
   var app = angular.module('timetracker', ['timeFilter']);
 
-  app.controller('TrackingController', function(){
-    this.tasks = tasks;
-    this.newTask = {};
-    this.currentTask = {};
+  app.controller('TrackingController', function($scope){
+    $scope.tasks = tasks;
+    $scope.newTask = {};
+    $scope.currentTask = {};
 
-    this.setCurrentTask = function(task) {
+    $scope.setCurrentTask = function(task) {
       //Add new task
-      this.tasks.push(task);
+      $scope.tasks.push(task);
       //Set CurrentTask
-      this.currentTask = this.newTask;
+      $scope.currentTask = $scope.newTask;
       //Clear new task
-      this.newTask = {};
+      $scope.newTask = {};
     };
-
   });
 
-
-  app.controller('NewMeasurementController', function(){
-  });
 
   var tasks = [
     {
